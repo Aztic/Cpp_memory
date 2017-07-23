cfca15e0249f802f387649fa84ede17b981fc6f8
Jhosevic Castañeda <zetsubou97@gmail.com>
2017-07-23 10:17:39 -0400

    Add files via upload

--- memory.cpp
+++ memory.cpp
@@ -15,7 +15,7 @@ Table::Table(){
 	this->words_per_way=0;
 }
 
-Table::Table(int ways, int words, int block_size,string method){
+Table::Table(int ways, int words, int block_size,std::string method,bool totally){
 	//reserve the memory for less painfull blocks append
 	this->ways.reserve(ways);
 	this->words = words;
@@ -23,10 +23,11 @@ Table::Table(int ways, int words, int block_size,string method){
 	this->words_per_way=words/ways;
 	this->blocks_per_way=words_per_way/block_size;
 	this->method = method;
+	this->is_associative = totally;
 	transform(this->method.begin(),this->method.end(),this->method.begin(),::toupper);
 	//Fill the "table" with "empty" vectors
 	for(int i=0;i<ways;i++){
-		vector<Block> temp;
+		std::vector<Block> temp;
 		temp.reserve(blocks_per_way);
 		for(int j=0;j<blocks_per_way;j++){
 			//Block temp(block_size);
@@ -35,47 +36,90 @@ Table::Table(int ways, int words, int block_size,string method){
 		this->ways.push_back(temp);
 	}
 	//Replacement functions info
-	this->rep_info.reserve(blocks_per_way);
-	for(int i=0;i<blocks_per_way;i++){
-		this->rep_info.push_back(Info(ways));
+	if(!totally){
+		this->rep_info.reserve(blocks_per_way);
+		for(int i=0;i<blocks_per_way;i++){
+			this->rep_info.push_back(Info(ways));
+		}		
 	}
+	else{
+		this->associative_info.init(ways,this->blocks_per_way);
+	}
+
 }
 
+//Return a vector of integer with the following structure
+//{block_address,way}
 void Table::insert_value(int value){
 	this->total++;
 	//Necessary values
 	int temp_value = value/this->block_size;
 	int block_address = temp_value%this->blocks_per_way;
 	int position = value%this->block_size;
-	//Check if the value is in any way
-	int index = 0;
 	bool found = false;
-	while(index < this->ways.size() && !found){
-		if(value == this->ways[index][block_address].position[position]){
-			this->hits++;
-			found = true;
+	//Check if the value is in any way
+	if(!this->is_associative){
+		int index = 0;
+		
+		while(index < this->ways.size() && !found){
+			if(value == this->ways[index][block_address].position[position]){
+				this->hits++;
+				found = true;
+			}
+			index++;
 		}
-		index++;
-	}
-	if(!found){
-		index = this->all_occuped(block_address);
-		if(index == -1){
-			//Replace
-			index = ((*this).*rep_funct[this->method])(block_address);
+		if(!found){
+			index = this->all_occuped(block_address);
+			if(index == -1){
+				//Replace
+				index = ((*this).*rep_funct[this->method])(block_address);
+			}
+			this->fill_block(this->ways[index][block_address],position,value);
+			this->update_info(block_address,index,true);
 		}
-		this->fill_block(this->ways[index][block_address],position,value);
-		this->update_info(block_address,index,true);
+		else{
+			index--;
+			this->update_info(block_address,index,false);
+		}
+		this->history.push_back(Element(value,temp_value,block_address,position,index,found));		
 	}
 	else{
-		index--;
-		this->update_info(block_address,index,false);
+       	int ind1=0;
+       	int ind2=0;
+       	while(ind1 < this->blocks_per_way && !found){
+           	ind2=0;
+           	while(ind2<this->ways.size() && !found){
+               	if(value == this->ways[ind2][ind1].position[position]){
+                   	found = true;
+                   	this->hits++;
+                   	ind2--;
+                   	ind1--;
+               	}
+               	ind2++;
+           }
+           ind1++;
+       	}
+       	
+       	if(!found){
+        	std::tuple<int,int> tup = this->all_occuped_as();
+        	if(std::get<0>(tup) == -1){
+            	tup = ((*this).*as_rep_funct[this->method])();
+        	}
+        	this->fill_block(this->ways[std::get<1>(tup)][std::get<0>(tup)],position,value);
+        	this->update_as_info(std::get<0>(tup),std::get<1>(tup),true);
+       	}
+       	else{
+        	this->update_as_info(ind1,ind2,false);
+       	}
+       	this->history.push_back(Element(value,-1,ind2,position,ind1,found));
 	}
+
 	/* Create for a more decent table */
-	this->history.push_back(Element(value,temp_value,block_address,position,index,found));
+	
 	/* ----------------------------- */
 }
 
-//Check if the block is occuped int all ways
+//Check if the block is occuped in all ways
 //either way, return the index of the way with free block
 int Table::all_occuped(int address){
 	for(int i=0;i<this->ways.size();i++){
@@ -85,16 +129,16 @@ int Table::all_occuped(int address){
 	return -1;
 }
 
-void Table::change_method(string new_method){
+void Table::change_method(std::string new_method){
 	this->method = new_method;
 	transform(this->method.begin(),this->method.end(),this->method.begin(),::toupper);
 }
 
 void Table::update_info(int ad, int w,bool replacing){
 	//Inserting order
-	vector<int> &order = this->rep_info[ad].element_order;
-	vector<int> &frecuency = this->rep_info[ad].frecuency;
-	vector<int> &used = this->rep_info[ad].used_order;
+	std::vector<int> &order = this->rep_info[ad].element_order;
+	std::vector<int> &frecuency = this->rep_info[ad].frecuency;
+	std::vector<int> &used = this->rep_info[ad].used_order;
 	int max_v = *max_element(order.begin(),order.end());
 	//All ways filled
 	/*Assumming a cache of 5 ways with the following inserting order
@@ -173,9 +217,136 @@ double Table::fail_percent(){
 }
 
 void Table::print_table(){
-	vector<Element> &t = this->history;
-	cout << "ref\t" << "dir1\t" << "dir2\t" << "dir3\t" << "way\t" << "hit\n";
+	std::vector<Element> &t = this->history;
+	std::cout << "ref\t" << "dir1\t" << "dir2\t" << "dir3\t" << "way\t" << "hit\n";
 	for(int i=0;i<this->history.size();i++){
-		cout << t[i].refi << "\t" << t[i].dir1 << "\t" << t[i].dir2 << "\t" << t[i].dir3 << "\t" << t[i].way << "\t" << t[i].hit << "\n";
+		std::cout << t[i].refi << "\t" << t[i].dir1 << "\t" << t[i].dir2 << "\t" << t[i].dir3 << "\t" << t[i].way << "\t" << t[i].hit << "\n";
 	}
+}
+
+/** Totally Associative Functions **/
+
+/** --- Max/Min element of matrix **/
+int Table::max_as_element(std::vector<std::vector<int> > &v){
+    int current = 0;
+    for(unsigned int i=0;i<v.size();i++){
+        for(unsigned int j=0;j<v[i].size();j++){
+            if(v[i][j] > current)
+                current = v[i][j];
+        }
+    }
+    return current;
+}
+
+int Table::min_as_element(std::vector<std::vector<int> > &v){
+    int current = INT_MAX;
+    for(unsigned int i = 0;i<v.size();i++){
+        for(unsigned int j=0;j<v[i].size();j++){
+            if(v[i][j] < current){
+                current = v[i][j];
+            }
+        }
+    }
+    return current;
+}
+
+void Table::update_as_info(int pos, int w, bool replacing){
+    //Inserting order
+    std::vector<std::vector<int> > &order = this->associative_info.element_order;
+    std::vector<std::vector<int> > &frecuency = this->associative_info.frecuency;
+    std::vector<std::vector<int> > &used = this->associative_info.used_order;
+    int max_v = max_as_element(order);
+    //All blocks filled
+    if(max_v == this->blocks_per_way * this->ways.size()){
+        for(int i=0;i<this->blocks_per_way;i++){
+            for(int j=0;j<this->ways.size();j++){
+                if(replacing && order[i][j] > order[pos][w]){
+                    order[i][j]--;
+                }
+                if(used[i][j] > used[pos][w]){
+                    used[i][j]--;
+                }
+            }
+
+        }
+        if(replacing) order[pos][w] = max_v;
+        used[pos][w] = max_v;
+    }
+    else{
+        if(replacing) order[pos][w] = max_v+1;
+        used[pos][w] = max_v+1;
+    }
+    //Used order
+
+    //Frecuency order
+    if(replacing){
+        frecuency[pos][w] = 1;
+    }
+    else{
+        frecuency[pos][w]++;
+    }
+}
+
+
+std::tuple<int,int> Table::all_occuped_as(){
+   	for(int i=0;i<this->blocks_per_way;i++){
+       	for(int j=0;j<this->ways.size();j++){
+           	if(this->ways[j][i].position[0] == -1) return std::make_tuple(i,j);
+       	}
+   	}
+   return std::make_tuple(-1,-1);
+}
+
+std::tuple<int,int> Table::as_random(){
+    time_t sec;
+    time(&sec);
+    srand((unsigned int) sec);
+    return std::make_tuple(rand()%this->blocks_per_way,rand()%this->ways.size());
+
+}
+std::tuple<int,int> Table::as_fifo(){
+    for(int i=0;i<this->blocks_per_way;i++){
+        for(int j=0;j<this->ways.size();j++){
+            if(this->associative_info.element_order[i][j] == 1)
+                return std::make_tuple(i,j);
+        }
+
+    }
+    return std::make_tuple(0,0);
+
+}
+
+std::tuple<int,int> Table::as_lifo(){
+    for(int i=0;i<this->blocks_per_way;i++){
+        for(int j=0;j<this->ways.size();j++){
+            if(this->associative_info.element_order[i][j] == this->blocks_per_way*this->ways.size())
+                return std::make_tuple(i,j);
+        }
+    }
+    return std::make_tuple(0,0);
+
+}
+
+std::tuple<int,int> Table::as_lfu(){
+    int min_value = min_as_element(this->associative_info.frecuency);
+    for(int i=0;i<this->blocks_per_way;i++){
+        for(int j=0;j<this->ways.size();j++){
+            if(this->associative_info.frecuency[i][j] == min_value)
+                return std::make_tuple(i,j);
+        }
+
+    }
+    return std::make_tuple(0,0);
+
+}
+std::tuple<int,int> Table::as_lru(){
+    for(int i=0;i<this->blocks_per_way;i++){
+        for(int j=0;j<this->ways.size();j++){
+            if(this->associative_info.used_order[i][j] == 1)
+                return std::make_tuple(i,j);
+        }
+
+    }
+    return std::make_tuple(0,0);
+
 }
--- memory.hpp
+++ memory.hpp
@@ -7,16 +7,19 @@
 #include <ctime>
 #include <vector>
 #include <map>
+#include <tuple>
+#include <climits>
 #define DEFAULT_VALUE -1
-using namespace std;
+
 
 class Table{
 private:
   	typedef int (Table::*ReplaceFunction)(int);
+  	typedef std::tuple<int,int> (Table::*AsReplaceFunction)(void);
   /* ------------- Data Structures ------------- */
 	//blocks structure
 	struct Block{
-		vector<int> position;
+		std::vector<int> position;
 		Block(int quantity){
 			this->position.reserve(quantity);
 			for(int i=0;i<quantity;i++){
@@ -48,54 +51,94 @@ private:
   	};
   //Replacement politics info
   	struct Info{
-	    vector<int> element_order;
-	    vector<int> used_order;
-	    vector<int> frecuency;
+	    std::vector<int> element_order;
+	    std::vector<int> used_order;
+	    std::vector<int> frecuency;
 	    Info(int ways){
 	      this->element_order.resize(ways);
 	      this->used_order.resize(ways);
 	      this->frecuency.resize(ways);
 	    }
 	};
+
+	struct AsInfo{
+		std::vector<std::vector<int> > element_order;
+		std::vector<std::vector<int> > used_order;
+		std::vector<std::vector<int> > frecuency;
+		void init(int w,int blocks){
+			this->element_order.resize(blocks);
+			this->used_order.resize(blocks);
+			this->frecuency.resize(blocks);
+			for(int i=0;i<blocks;i++){
+				this->element_order[i].resize(w);
+				this->used_order[i].resize(w);
+				this->frecuency[i].resize(w);
+			}
+		}
+	};
   /* ------------------------------------------- */
 
   /* ------------ Functions/Procedures---------- */
 	void fill_block(Block &b, int position, int value);
 	int all_occuped(int address);
-	void change_method(string new_method);
+	void change_method(std::string new_method);
+	//No Associative functions
 	int random(int index);
 	int lfu(int index);
 	int lru(int index);
 	int fifo(int index);
 	int lifo(int index);
 	void update_info(int ad,int w,bool replacing);
+
+	//Associative functions
+	std::tuple<int,int> as_random();
+	std::tuple<int,int> as_lfu();
+	std::tuple<int,int> as_lru();
+	std::tuple<int,int> as_fifo();
+	std::tuple<int,int> as_lifo();
+	void update_as_info(int ad,int w,bool replacing);
+	std::tuple<int,int> all_occuped_as();
+	int max_as_element(std::vector<std::vector<int> > &v);
+	int min_as_element(std::vector<std::vector<int> > &v);
   /* ------------------------------------------- */
 
 
   /*-------------- Data section ---------------- */
-	vector<vector<Block> > ways;
-	string mode;
+	std::vector<std::vector<Block> > ways;
+	std::string mode;
 	int words;
 	int block_size;
 	int words_per_way;
 	int blocks_per_way;
   	int total=0;
 	int hits=0;
-  	vector<Info> rep_info;
- 	vector<Element> history;
-  	string method;
- 	map<string,ReplaceFunction> rep_funct={
+	bool is_associative;
+  	std::vector<Info> rep_info;
+ 	std::vector<Element> history;
+ 	AsInfo associative_info;
+  	std::string method;
+  	//No-associative replacement functions
+ 	std::map<std::string,ReplaceFunction> rep_funct={
  	 	{"RANDOM",&Table::random},
  	 	{"LFU",&Table::lfu},
  	 	{"LRU",&Table::lru},
  	 	{"FIFO",&Table::fifo},
  	 	{"LIFO",&Table::lifo}
  	};
+
+ 	//Associative replacement functions
+ 	std::map<std::string,AsReplaceFunction> as_rep_funct ={
+ 		{"RANDOM",&Table::as_random},
+ 		{"LFU",&Table::as_lfu},
+ 		{"LRU",&Table::as_lru},
+ 		{"FIFO",&Table::as_fifo},
+ 		{"LIFO",&Table::as_lifo}
+ 	};
   /* ------------------------------------------- */
 
 public:
 	Table();
-	Table(int ways,int words, int block_size,string method);
+	Table(int ways,int words, int block_size,std::string method,bool totally);
 	void insert_value(int value);
   	double hit_percent();
   	double fail_percent();
